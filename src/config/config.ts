a6a4505294eb0cf7e119512a804b2e74b6cd163c
Andrew Vaverchak <avaver@gmail.com>
2018-03-17 20:32:20 +0200

    old matches are not processed anymore after application startup

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,7 @@
 export const OPENDOTA_MIN_REQUEST_FREQ_MS = 500;
 
 export const MATCH_POLL_INTERVAL_MS = 60 * 1000;
+export const MATCH_RECENT_THRESHOLD_MS = 60 * 60 * 1000;
 
 export const DISCORD_BOSS_ID = "407971834689093632";
 export const BIBA_MIN_SIZE = 1;
--- src/services/match-service.ts
+++ src/services/match-service.ts
@@ -2,19 +2,20 @@
 
 import * as log from 'log4js';
 import { OpenDota } from '../api/opendota';
-import { Match, Profile, Hero } from '../model/opendota-types';
+import { Match, Profile, Hero, RecentMatch } from '../model/opendota-types';
 import { Observable } from 'rxjs';
 import { AccountService } from './account-service';
 import { HeroService } from './hero-service';
-import { MATCH_POLL_INTERVAL_MS } from '../config/config';
+import { MATCH_POLL_INTERVAL_MS, MATCH_RECENT_THRESHOLD_MS } from '../config/config';
 
 export namespace MatchService {
   const logger = log.getLogger('match-service');
 
   export function getMatchStream(): Observable<Match> {
     return Observable.interval(MATCH_POLL_INTERVAL_MS)
-      .withLatestFrom(AccountService.getAccounts()).map(val => val[1])
+      .withLatestFrom(AccountService.getAccounts()).map(val => { logger.debug('checking for matches'); return val[1]; })
       .switchMap(accs => accs.map(a => OpenDota.getLastMatchForPlayer(a.account_id))).mergeAll().distinct(m => m.match_id)
+      .filter(m => recentMatch(m))
       .flatMap(m => OpenDota.getMatchDetails(m.match_id))
       .withLatestFrom(AccountService.getAccounts(), HeroService.getHeroes())
       .map(val => transformMatch(val[0], val[1], val[2]));
@@ -34,5 +35,16 @@ export namespace MatchService {
 
     logger.debug('match %s - players: %s', match.match_id, match.players.map(p => p.personaname).join(', '));
     return match;
+  }
+
+  function recentMatch(match: RecentMatch): boolean {
+    const matchEndTime = (match.start_time + match.duration) * 1000;
+    const isRecent = Date.now() - matchEndTime < MATCH_RECENT_THRESHOLD_MS;
+    if (!isRecent) {
+      logger.debug('skipping match %s as too old (finished on %s)', 
+        match.match_id, 
+        new Date(matchEndTime).toISOString());
+    }
+    return isRecent;
   }
 }
