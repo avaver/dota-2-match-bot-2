fca8e6b49bcf22f36d24ce4a648c0bb1335cbf78
Andriy Vaverchak <anv@csia.net>
2018-03-23 18:24:07 +0200

    added sentry for errors monitoring

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,41 @@
 'use strict';
 
 import * as log from 'log4js';
+import * as raven from 'raven';
+import { SENTRY_URL } from './config/secure-config';
 
-const logLayout = process.env['HEROKU'] ? 
-  { type: 'pattern', pattern: '[%p] %c - %m' } : 
-  { type: 'colored' };
-log.configure({
-  appenders: { out: { type: 'stdout', layout: logLayout } },
-  categories: { default: { appenders: ['out'], level: 'debug' } }
-});
+let sentry = new raven.Client(SENTRY_URL);
+sentry.install();
+
+configureLogger();
 const logger = log.getLogger();
 
 import { MatchService } from './services/match-service';
-import MessageService from './services/message-service';
+import { MessageService } from './services/message-service';
 import { DiscordWebhook } from './api/discord-webhook';
 import BotService from './services/bot-service';
 
 const botService = new BotService();
-const messageService = new MessageService();
 
 botService.run();
 
 MatchService.getMatchStream()
-  .map(match => messageService.getMatchSummaryMessage(match))
-  .subscribe(DiscordWebhook.post, logger.error);
+  .map(MessageService.getMatchSummaryMessage)
+  .subscribe(DiscordWebhook.post, handleError);
 
 logger.info('application started');
+
+function configureLogger(): void {
+  const logLayout = process.env['HEROKU'] ? 
+    { type: 'pattern', pattern: '[%p] %c - %m' } : 
+    { type: 'colored' };
+  log.configure({
+    appenders: { out: { type: 'stdout', layout: logLayout } },
+    categories: { default: { appenders: ['out'], level: 'debug' } }
+  });
+}
+
+function handleError(error: any): void {
+  logger.error(error); 
+  raven.captureException(error);
+}
