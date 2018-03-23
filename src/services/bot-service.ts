4b8fdef9ba99be1ed52019b041797411b9271459
Andriy Vaverchak <anv@csia.net>
2018-03-23 18:25:36 +0200

    one processor instead of new instance for each incoming command

--- src/services/bot-service.ts
+++ src/services/bot-service.ts
@@ -16,28 +16,23 @@ import MmrCommand from './bot-commands/mmr-command';
 import ShitcannonCommand from './bot-commands/shitcannon-command';
 import { format } from 'util';
 
-export default class BotService {
+export default class BotService implements Processor {
   private logger = log.getLogger('bot-service');
   private client = new Client();
   private analyzers = new Array<Processor>();
 
-  private processors2: Map<string, new() => Processor> = new Map([
-    ['bibametr', BibametrCommand],
-    ['watchlist', WatchlistCommand],
-    ['register', RegisterCommand],
-    ['unregister', UnregisterCommand],
-    ['фас', SwearCommand],
-    ['shitcannon', ShitcannonCommand],
-    ['mmr', MmrCommand],
-    ['clear', ClearCommand]
-  ]);
-
-  private createProcessor<T extends Processor>(type: new() => T): T {
-    return new (type)();
-  }
+  private processors = new Map<string, Processor>();
 
   public run(): void {
-
+    this.processors.set('bibametr', new BibametrCommand());
+    this.processors.set('clear', new ClearCommand());
+    this.processors.set('commands', this);
+    this.processors.set('mmr', new MmrCommand());
+    this.processors.set('register', new RegisterCommand());
+    this.processors.set('shitcannon', new ShitcannonCommand());
+    this.processors.set('unregister', new UnregisterCommand());
+    this.processors.set('watchlist', new WatchlistCommand());
+    this.processors.set('фас', new SwearCommand());
     this.analyzers.push(new NaviProcessor());
 
     this.client.on('ready', () => this.onReady());
@@ -56,16 +51,16 @@ export default class BotService {
       let command = this.getCommand(message.content);
       if (command) {
         this.logger.info('processing bot command %s, sent by %s in %s|%s', this.getCommand(message.content), message.author.username, message.guild.name, channel.name);
-        if (command === 'commands') {
-          let commands = [...this.processors2].map(p => '!' + p[0]).sort().join('\n');
-          message.channel.send(format('```diff\n%s\n```', commands));
-        } else {
-          let processorType = this.processors2.get(command) || DefaultCommand;
-          this.createProcessor(processorType).process(message);
-        }
+        let processor = this.processors.get(command) || new DefaultCommand();
+        processor.process(message);
       }
       this.analyzers.forEach(analyzer => analyzer.process(message));
     }
+  }
+
+  public process(message: Message) {
+    let commands = [...this.processors].map(p => '!' + p[0]).sort().join('\n');
+    message.channel.send(format('```diff\n%s\n```', commands));
   }
 
   private getCommand(content: string): string | undefined {
