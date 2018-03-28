b62cd4ad636a32e1e8071f44624652a25208b076
Andriy Vaverchak <anv@csia.net>
2018-03-28 21:16:51 +0300

    fixed issue with sentry and restructured app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,18 @@
 'use strict';
 
 import * as log from 'log4js';
-import * as raven from 'raven';
-import { SENTRY_URL } from './config/secure-config';
-
-let sentry = new raven.Client(SENTRY_URL);
-sentry.install();
-
 configureLogger();
 const logger = log.getLogger();
+
+import * as raven from 'raven';
+import { SENTRY_URL } from './config/secure-config';
+let sentry = new raven.Client(SENTRY_URL);
+sentry.install();
 
 import { MatchService } from './services/match-service';
 import { MessageService } from './services/message-service';
 import { DiscordWebhook } from './api/discord-webhook';
 import BotService from './services/bot-service';
-
-const botService = new BotService();
-
-botService.run();
-
-MatchService.getMatchStream()
-  .map(MessageService.getMatchSummaryMessage)
-  .subscribe(DiscordWebhook.post, handleError);
-
-logger.info('application started');
 
 function configureLogger(): void {
   const logLayout = process.env['HEROKU'] ? 
@@ -31,11 +20,16 @@ function configureLogger(): void {
     { type: 'colored' };
   log.configure({
     appenders: { out: { type: 'stdout', layout: logLayout } },
-    categories: { default: { appenders: ['out'], level: 'debug' } }
+    categories: { default: { appenders: ['out'], level: 'trace' } }
   });
 }
 
 function handleError(error: any): void {
   logger.error(error); 
-  raven.captureException(error);
+  sentry.captureException(error);
 }
+
+new BotService().run();
+MatchService.getMatchStream().map(MessageService.getMatchSummaryMessage).subscribe(DiscordWebhook.post, handleError);
+
+logger.info('application started');
