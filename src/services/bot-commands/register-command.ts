ad15ebc7572691e4ee5b44819428a29c341ae4ed
Andriy Vaverchak <anv@csia.net>
2018-03-28 21:17:45 +0300

    added retry logic and changed Observables to Promises

--- src/services/bot-commands/register-command.ts
+++ src/services/bot-commands/register-command.ts
@@ -11,14 +11,12 @@ export default class RegisterCommand extends CommandBase {
     let args = this.getArgs(message.content);
     let id = parseInt(args[0].trim());
     if (args.length > 0 && id) {
-      ProfileService.getProfile(id).take(1).subscribe(profile => {
-        if (profile) {
-          Firebase.addAccount(id);
-          message.channel.send(format('```account %s (%s) registered```', id, profile.personaname));
-        } else {
-          message.channel.send(format('```account %s not found```', id));
-        }
-      });
+      ProfileService.getProfile(id)
+        .then(profile => {
+            Firebase.addAccount(id);
+            message.channel.send(format('```account %s (%s) registered```', id, profile.personaname));
+        })
+        .catch(() => message.channel.send(format('```account %s not found```', id)));
     } else {
       this.logger.warn('incorrect parameters for command %s. expected number, actual: %s', this.getCommand(message.content), message.content);
     }
