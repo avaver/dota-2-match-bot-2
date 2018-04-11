ba5cf6d66d2f74ac89f0206acd79f39eb3997a7f
Andriy Vaverchak <avaver@gmail.com>
2018-04-11 16:54:28 +0300

    added logic to store published matches

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,8 +2,9 @@
 
 import * as log from 'log4js';
 import * as api from 'firebase-admin';
-import { FIREBASE_DB_URL, FIREBASE_CERT } from '../config/secure-config';
 import { Observable, Subject } from 'rxjs';
+import { FIREBASE_DB_URL, FIREBASE_CERT } from '../config/secure-config';
+import { PUBLISHED_MATCH_STORAGE_S } from '../config/config';
 
 const logger = log.getLogger('firebase');
 
@@ -12,14 +13,6 @@ api.initializeApp({
   credential: api.credential.cert(FIREBASE_CERT),
   databaseURL: FIREBASE_DB_URL
 });
-
-// TODO: move to Firestore
-/*
-const db = api.firestore();
-db.collection('accounts').get().then(s => {
-  console.log(s.docs.map(d => d.data()));
-});
-*/
 
 export namespace Firebase {
   const subjects = new Map<string, Subject<any>>();
@@ -35,7 +28,7 @@ export namespace Firebase {
         if (snapshot) {
           let value = snapshot.val() as T;
           if (value) {
-            logger.info('firebase data changed at "%s"', path);
+            logger.debug('firebase data changed at "%s"', path);
             subject.next(value);
           }
         }
@@ -85,6 +78,25 @@ export namespace Firebase {
   }
 
   export function getAccounts(): Promise<number[]> {
+    logger.trace('requesting accounts');
     return api.database().ref('accounts').once('value').then(snapshot => snapshot.val() as number[]);
+  }
+
+  export function addPublishedMatch(id: number, endTime: number): Promise<void> {
+    logger.trace('saving published match %s finished at %s', id, new Date(endTime * 1000).toISOString());
+    return api.database().ref('matches/' + id).set(endTime).then(() => cleanupOldPublishedMatches());
+  }
+
+  export function cleanupOldPublishedMatches(): Promise<void> {
+    logger.trace('cleaning up outdated published matches');
+    return api.database().ref('matches')
+      .orderByValue()
+      .endAt(Math.floor(Date.now() / 1000 - PUBLISHED_MATCH_STORAGE_S))
+      .once('value').then(snapshot => snapshot.forEach((child: api.database.DataSnapshot) => { child.ref.remove(); return false; }));
+  }
+
+  export function getRecentPublishedMatches(): Promise<number[]> {
+    logger.trace('requesting recently published matches');
+    return api.database().ref('matches').once('value').then(snapshot => snapshot.val() ? Object.keys(snapshot.val()).map(key => parseInt(key)) : []);
   }
 }
