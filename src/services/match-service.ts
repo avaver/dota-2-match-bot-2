1ae7150b314fdbd485d92ddba19c515275a0bb4c
Andriy Vaverchak <avaver@gmail.com>
2018-04-16 18:14:38 +0300

    changed log level

--- src/services/match-service.ts
+++ src/services/match-service.ts
@@ -7,58 +7,40 @@ import { Account } from '../model/matchbot-types';
 import { Observable } from 'rxjs';
 import { AccountService } from './account-service';
 import { HeroService } from './hero-service';
-import { MATCH_POLL_INTERVAL_MS, MATCH_RECENT_THRESHOLD_MS } from '../config/config';
-import { PublishedService } from './published-service';
+import { MATCH_POLL_INTERVAL_MS } from '../config/config';
 import { DB } from '../api/firebase-db';
+
+class MatchStreamData {
+  constructor(public accounts: Account[], public heroes: Hero[], public match: Match | RecentMatch, public currentAccount: number) {}
+}
 
 export namespace MatchService {
   const logger = log.getLogger('match-service');
 
   export function getMatchStream(): Observable<Match> {
     return Observable.interval(MATCH_POLL_INTERVAL_MS)
-      .withLatestFrom(AccountService.getAccounts())
-      .map(val => { logger.trace('checking for matches'); return val[1]; })
-      .switchMap(accs => accs.map(a => Observable.fromPromise(OpenDota.getLastMatchForPlayer(a.id)))).mergeAll()
-      .distinct(m => m.match_id)
-      .withLatestFrom(PublishedService.getMatches())
-      .filter(val => recentMatch(val[0], val[1]))
-      .map(val => val[0])
-      .flatMap(m => OpenDota.getMatchDetails(m.match_id))
+      .do(() => logger.debug('checking for new matches'))
       .withLatestFrom(AccountService.getAccounts(), HeroService.getHeroes())
-      .map(val => transformMatch(val[0], val[1], val[2]))
-      .catch((error, source) => { logger.error('Error getting match stream: %s. Trying to recover', error.message); return source; });
-  }
-
-  function transformMatch(match: Match, accounts: Account[], heroes: Hero[]): Match {
-    logger.info('processing match %s', match.match_id);
-    
-    // remove other players
-    match.players = match.players.filter(player => accounts.map(account => account.id).indexOf(player.account_id) >= 0);
-
-    // add profile and hero information
-    match.players.forEach(player => {
-      player.profile = accounts.find(account => account.id == player.account_id)!.profile;
-      player.hero = heroes.find(hero => hero.id == player.hero_id);
-    });
-
-    // save match as already published
-    DB.addPublishedMatch(match.match_id, match.start_time + match.duration);
-
-    logger.info('match %s - players: %s', match.match_id, match.players.map(p => p.personaname).join(', '));
-    return match;
-  }
-
-  function recentMatch(match: RecentMatch, publishedMatches: number[]): boolean {
-    const matchEndTime = (match.start_time + match.duration) * 1000;
-    let isRecent = true;
-    if (Date.now() - matchEndTime > MATCH_RECENT_THRESHOLD_MS) {
-      logger.info('skipping match %s as too old (finished on %s)', match.match_id, new Date(matchEndTime).toISOString());
-      isRecent = false;
-    } else if (publishedMatches.indexOf(match.match_id) != -1) {
-      logger.info('skipping match %s as already published', match.match_id);
-      isRecent = false;
-    }
-
-    return isRecent;
+      .do(() => logger.trace('loaded accounts and heroes'))
+      .switchMap(data => data[1].map(account => Observable.fromPromise(OpenDota.getLastMatchForPlayer(account.id).then(recentMatch => new MatchStreamData(data[1], data[2], recentMatch, account.id))))).mergeAll()
+      .do(data => logger.trace('checking match %s', data.match.match_id))
+      .distinct(data => data.match.match_id)
+      .do(data => logger.trace('match %s passed distinct check', data.match.match_id))
+      .filter(data => data.accounts.map(account => account.lastMatchId).indexOf(data.match.match_id) === -1)
+      .do(data => logger.trace('match %s passed latest matches check', data.match.match_id))
+      .do(data => DB.updateLastMatch(data.currentAccount, data.match.match_id))
+      .do(data => logger.trace('updated last match id for account %s', data.currentAccount))
+      .flatMap(data => OpenDota.getMatchDetails(data.match.match_id), (data, matchDetails) => { data.match = matchDetails; return data; })
+      .do(data => logger.trace('loaded match %s details', data.match.match_id))
+      .map(data => {
+        let match = data.match as Match;
+        match.players = match.players.filter(player => data.accounts.map(account => account.id).indexOf(player.account_id) >= 0);
+        match.players.forEach(player => {
+          player.profile = data.accounts.find(account => account.id === player.account_id)!.profile;
+          player.hero = data.heroes.find(hero => hero.id === player.hero_id);
+          logger.trace('resolved profile and hero information for account %s (%s, %s)', player.account_id, player.profile!.personaname, player.hero!.localized_name);
+        });
+        return match;
+      });
   }
 }
